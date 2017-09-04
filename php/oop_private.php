ca3e427abdff22700e535cb93b4fc4d1044b0c89
sunkeyi <296675685@qq.com>
2017-09-04 09:01:41 +0800

    完善魔术方法

--- php/oop_private.php
+++ php/oop_private.php
@@ -16,11 +16,23 @@ class Test {
 		$this->$propertyName = $value;
 		return;
 	}
+
+	public function __isset($propertyName) {
+		return isset($this->$propertyName);
+	}
+
+	public function __unset($propertyName) {
+		echo '删除属性' . "\n" . $propertyName . "\n的值";
+		unset($this->$propertyName);
+		return;
+	}
 }
 
 $test = new Test();
-$test->__set($name, 'ken');
-echo $test->__get($name); // ken
+$test->$name = 'ken';
+echo $test->$name; // ken
+$test->__unset($name);
+echo $test->$name;
 // 之所以做这个测试，是因为看到深入理解yii2.0里，作者认为只有设置为共有属性，才能使用
 // 魔术方法赋值
 // 在整个yii框架中，经常通过魔术方法进行依赖注入，控制反转等，
