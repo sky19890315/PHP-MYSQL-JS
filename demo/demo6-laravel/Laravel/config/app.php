e56a2eac3719996ee6ffc336c5d5e796c0d34261
sunkeyi <296675685@qq.com>
2017-09-13 19:54:46 +0800

    紧急修复

--- demo/demo6-laravel/Laravel/config/app.php
+++ demo/demo6-laravel/Laravel/config/app.php
@@ -11,7 +11,6 @@ return [
     | stack traces will be shown on every error that occurs within your
     | application. If disabled, a simple generic error page is shown.
     |
-    */
      */
 
     'debug'           => env('APP_DEBUG', false),
